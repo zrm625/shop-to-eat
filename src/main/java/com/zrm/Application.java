7b6e8d11ac1e77f1539ed2ce354d563cb4b9e8ee
Zack <36251971+zrm625@users.noreply.github.com>
2020-05-09 23:50:30 -0400

    Update Application.java

--- src/main/java/com/zrm/Application.java
+++ src/main/java/com/zrm/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication(scanBasePackages = {"com.zrm"})
+@SpringBootApplication(scanBasePackages = { "com.zrm" })
 @EnableJpaAuditing
 public class Application {
 
