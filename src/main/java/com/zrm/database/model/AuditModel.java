4ab040e9384739d7739999163b63aa7b896cf91a
Zack <36251971+zrm625@users.noreply.github.com>
2020-05-09 22:16:20 -0400

    refactor

--- src/main/java/com/zrm/database/model/AuditModel.java
+++ src/main/java/com/zrm/database/model/AuditModel.java
@@ -10,10 +10,7 @@ import java.util.Date;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-@JsonIgnoreProperties(
-        value = {"createdAt", "updatedAt"},
-        allowGetters = true
-)
+@JsonIgnoreProperties(value = { "createdAt", "updatedAt" }, allowGetters = true)
 public abstract class AuditModel implements Serializable {
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "created_at", nullable = false, updatable = false)
